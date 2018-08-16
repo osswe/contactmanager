8e9174ba657e567c9dbf61da3690a6c513443b39
Mike <mike@jadoti.com>
2018-08-15 23:15:48 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+// main app component that gets rendered
+// to the index.html dom element #root
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>The app component</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
