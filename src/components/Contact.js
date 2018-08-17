c6ce5d21dbc825518f9218724ceff31e8bab389c
mmccue <mmccue@paylocity.com>
2018-08-17 01:04:42 -0500

    finished 22.Events in React

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,11 +3,18 @@ import PropTypes from 'prop-types'
 
 class Contact extends Component {
 
+  state = {};
+
+  onShowClick = (event) => {
+    console.log('ha!', event.target);
+  }
+
   render() {
     const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
-        <h4>{ name }</h4>
+        <h4>{ name } <i onClick={ this.onShowClick } className="fas fa-sort-down"></i>
+        </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: { email }</li>
           <li className="list-group-item">Phone: { phone }</li>
